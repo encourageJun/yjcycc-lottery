25e5515f56982255a60d7fe5627f3798ecdfb84e
yangjun <yangjun@xingxingcar.com>
2019-08-30 18:53:25 +0800

    新增: 开奖和投注业务逻辑

--- yjcycc-lottery-common/src/main/java/org/yjcycc/lottery/entity/Plan.java
+++ yjcycc-lottery-common/src/main/java/org/yjcycc/lottery/entity/Plan.java
@@ -1,38 +1,42 @@
 package org.yjcycc.lottery.entity;
 
-import org.apache.ibatis.type.Alias;
-import org.yjcycc.tools.common.entity.BaseEntity;
-
 import java.io.Serializable;
 import java.math.BigDecimal;
+
+import org.apache.ibatis.type.Alias;
+import org.yjcycc.tools.common.entity.BaseEntity;
 
 /**
  * 投注计划(Plan)实体类
  *
  * @author yangjun
- * @since 2019-08-19 17:08:13
+ * @since 2019-08-23 18:26:34
  */
 @Alias("Plan")
 public class Plan extends BaseEntity implements Serializable {
-    private static final long serialVersionUID = 451696839505562177L;
+    private static final long serialVersionUID = -22781716297625028L;
     // 投注计划配置id
     private Long planConfigId;
+    // 彩票种类(字典标签label)
+    private String dictLotteryType;
     // 中奖状态, 字典:plan_status, 0追号中 1未中奖 2已中奖
     private Integer status;
-    // 当期投注金额
-    private BigDecimal amount;
-    // 当期投注倍数
-    private Integer doubleCount;
     // 当前投注期数
     private Integer stageCount;
     // 总期数
     private Integer totalStageCount;
-    // 累积金额
+    // 当期投注金额
+    private BigDecimal amount;
+    // 当期投注倍数
+    private Integer doubleCount;
+    // 累积投注金额
     private BigDecimal totalAmount;
     // 中奖金额
     private BigDecimal winAmount;
     // 盈利金额
     private BigDecimal profitAmount;
+    // 计划配置执行序号, 例如: 0,1,2,3..., 持续累加, 已有计划数据的情况下不可清零.(来自计划配置的executeIndex)
+    private Integer executeIndex;
 
     private PlanConfig planConfig;
 
@@ -45,28 +49,20 @@ public class Plan extends BaseEntity implements Serializable {
         this.planConfigId = planConfigId;
     }
 
+    public String getDictLotteryType() {
+        return dictLotteryType;
+    }
+
+    public void setDictLotteryType(String dictLotteryType) {
+        this.dictLotteryType = dictLotteryType;
+    }
+
     public Integer getStatus() {
         return status;
     }
 
     public void setStatus(Integer status) {
         this.status = status;
-    }
-
-    public BigDecimal getAmount() {
-        return amount;
-    }
-
-    public void setAmount(BigDecimal amount) {
-        this.amount = amount;
-    }
-
-    public Integer getDoubleCount() {
-        return doubleCount;
-    }
-
-    public void setDoubleCount(Integer doubleCount) {
-        this.doubleCount = doubleCount;
     }
 
     public Integer getStageCount() {
@@ -83,6 +79,22 @@ public class Plan extends BaseEntity implements Serializable {
 
     public void setTotalStageCount(Integer totalStageCount) {
         this.totalStageCount = totalStageCount;
+    }
+
+    public BigDecimal getAmount() {
+        return amount;
+    }
+
+    public void setAmount(BigDecimal amount) {
+        this.amount = amount;
+    }
+
+    public Integer getDoubleCount() {
+        return doubleCount;
+    }
+
+    public void setDoubleCount(Integer doubleCount) {
+        this.doubleCount = doubleCount;
     }
 
     public BigDecimal getTotalAmount() {
@@ -115,5 +127,13 @@ public class Plan extends BaseEntity implements Serializable {
 
     public void setPlanConfig(PlanConfig planConfig) {
         this.planConfig = planConfig;
+    }
+
+    public Integer getExecuteIndex() {
+        return executeIndex;
+    }
+
+    public void setExecuteIndex(Integer executeIndex) {
+        this.executeIndex = executeIndex;
     }
 }
--- yjcycc-lottery-common/src/main/java/org/yjcycc/lottery/entity/PlanConfig.java
+++ yjcycc-lottery-common/src/main/java/org/yjcycc/lottery/entity/PlanConfig.java
@@ -3,37 +3,45 @@ package org.yjcycc.lottery.entity;
 import org.apache.ibatis.type.Alias;
 import org.yjcycc.tools.common.entity.BaseEntity;
 
-import java.io.Serializable;
+import java.math.BigDecimal;
 import java.util.List;
 
 /**
  * 投注计划配置(PlanConfig)实体类
  *
  * @author yangjun
- * @since 2019-08-22 16:20:27
+ * @since 2019-08-30 10:29:03
  */
 @Alias("PlanConfig")
-public class PlanConfig extends BaseEntity implements Serializable {
-    private static final long serialVersionUID = 443241460619232599L;
+public class PlanConfig extends BaseEntity {
+    private static final long serialVersionUID = 299471688905797715L;
     // 金额模式, 字典:amount_model, 1元 10角 100分 1000厘
     private Integer dictAmountModel;
     // 倍投方案, 数据字典:double_scheme, 1 盈利率 2 单倍
     private Integer dictDoubleScheme;
+    // 彩票种类(字典标签label)
+    private String dictLotteryType;
     // 倍数
     private Integer doubleCount;
     // 选号方案, 数据字典:choose_scheme, 1 手动设置胆码-拖码 2 统计选胆码-随机选拖码
     private Integer dictChooseScheme;
-    // 追号方案id
-    private Long pursueId;
     // 胆码
     private String danNumber;
     // 拖码
     private String tuoNumber;
     // 占用状态, 0禁用 1否 2是
     private Integer isOccupy;
+    // 提现盈利率, 达到盈利率后提现
+    private Double cashOutRate;
+    // 计划配置执行序号, 例如: 0,1,2,3...
+    private Integer executeIndex;
+    // 充值金额
+    private BigDecimal recharge;
+    // 累积盈利
+    private BigDecimal totalProfitAmount;
 
     private List<PursueScheme> pursueList;
-
+    private List<Plan> planList;
 
     public Integer getDictAmountModel() {
         return dictAmountModel;
@@ -51,6 +59,14 @@ public class PlanConfig extends BaseEntity implements Serializable {
         this.dictDoubleScheme = dictDoubleScheme;
     }
 
+    public String getDictLotteryType() {
+        return dictLotteryType;
+    }
+
+    public void setDictLotteryType(String dictLotteryType) {
+        this.dictLotteryType = dictLotteryType;
+    }
+
     public Integer getDoubleCount() {
         return doubleCount;
     }
@@ -65,14 +81,6 @@ public class PlanConfig extends BaseEntity implements Serializable {
 
     public void setDictChooseScheme(Integer dictChooseScheme) {
         this.dictChooseScheme = dictChooseScheme;
-    }
-
-    public Long getPursueId() {
-        return pursueId;
-    }
-
-    public void setPursueId(Long pursueId) {
-        this.pursueId = pursueId;
     }
 
     public String getDanNumber() {
@@ -99,11 +107,51 @@ public class PlanConfig extends BaseEntity implements Serializable {
         this.isOccupy = isOccupy;
     }
 
+    public Double getCashOutRate() {
+        return cashOutRate;
+    }
+
+    public void setCashOutRate(Double cashOutRate) {
+        this.cashOutRate = cashOutRate;
+    }
+
+    public Integer getExecuteIndex() {
+        return executeIndex;
+    }
+
+    public void setExecuteIndex(Integer executeIndex) {
+        this.executeIndex = executeIndex;
+    }
+
+    public BigDecimal getRecharge() {
+        return recharge;
+    }
+
+    public void setRecharge(BigDecimal recharge) {
+        this.recharge = recharge;
+    }
+
+    public BigDecimal getTotalProfitAmount() {
+        return totalProfitAmount;
+    }
+
+    public void setTotalProfitAmount(BigDecimal totalProfitAmount) {
+        this.totalProfitAmount = totalProfitAmount;
+    }
+
     public List<PursueScheme> getPursueList() {
         return pursueList;
     }
 
     public void setPursueList(List<PursueScheme> pursueList) {
         this.pursueList = pursueList;
+    }
+
+    public List<Plan> getPlanList() {
+        return planList;
+    }
+
+    public void setPlanList(List<Plan> planList) {
+        this.planList = planList;
     }
 }
